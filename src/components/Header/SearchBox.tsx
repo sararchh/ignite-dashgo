bfba57330b64b2409b589cfccf967c54022ca821
Sara Rocha <sararocha@MacBook-Air-de-Codefique.local>
2022-05-03 19:34:17 -0300

    Form de autenticação

--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -1,7 +1,10 @@
 import { Flex, Icon, Input } from "@chakra-ui/react";
+import { useRef } from "react";
 import { RiSearchLine } from "react-icons/ri";
 
 export function SearchBox() {
+
+
   return (
     <Flex
       as='label'
