ff14198e78583e5b410db456ed9e00cf463e26ac
Sara Rocha <sararocha@MacBook-Air-de-Codefique.local>
2022-06-21 19:50:06 -0300

    SSR no React Query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -6,14 +6,17 @@ import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead
 import { RiAddLine } from 'react-icons/ri';
 
 import { Pagination } from "../../components/Pagination";
-import { useUsers } from "../../services/hooks/useUsers";
+import { getUsers, useUsers } from "../../services/hooks/useUsers";
 import { useState } from "react";
 import { queryClient } from "../../services/queryClient";
 import { api } from "../../services/api";
+import { GetServerSideProps } from "next";
 
-export default function UserList() {
+export default function UserList({ users }) {
   const [page, setPage] = useState(1); //pagina que se inicia
-  const { data, isLoading, isFetching, error } = useUsers(page);
+  const { data, isLoading, isFetching, error } = useUsers(page, {
+    initialData: users,
+  });
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -113,3 +116,13 @@ export default function UserList() {
     </Box>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { users, totalCount } = await getUsers(1)
+
+  return {
+    props: {
+      users,
+    }
+  }
+} 
